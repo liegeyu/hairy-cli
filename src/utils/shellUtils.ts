55115c46199598cdf660767734318a44e0f5f671
温良 <yulin.zhang@ideacome.com>
2023-07-18 17:40:42 +0800

    完善 init 命令

---
+++ src/utils/shellUtils.ts
@@ -0,0 +1,32 @@
+// shelljs's util
+import shell from "shelljs";
+
+/**
+ * 执行 shell 命令
+ * @param {String} command
+ * @return {Promise<Stirng>}
+ */
+export const execCommand = (command: String): Promise<String | Number> => {
+  return new Promise((resolve, reject) => {
+    shell.exec(
+      command,
+      { silent: true },
+      (code, stdout, stderr) => {
+        if (code === 0) {
+          resolve(code);
+        } else {
+          reject(new Error(stderr));
+        }
+    })
+  })
+}
+
+/**
+ * 删除文件或目录方法
+ * @param {String} path 
+ * @returns {Boolean}
+ */
+export const rmCommand = (path: String): Boolean => {
+  const res = shell.rm('-rf', path);
+  return res.code === 0;
+}
